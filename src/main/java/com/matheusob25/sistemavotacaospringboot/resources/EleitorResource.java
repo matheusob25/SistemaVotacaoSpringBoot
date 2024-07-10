23dbb974b2c3e9fe7389c8d636fbabb319443476
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-07-09 23:48:43 -0300

    métodos de listagem de politicos e eleitores funcionando

--- src/main/java/com/matheusob25/sistemavotacaospringboot/resources/EleitorResource.java
+++ src/main/java/com/matheusob25/sistemavotacaospringboot/resources/EleitorResource.java
@@ -4,6 +4,7 @@ import com.matheusob25.sistemavotacaospringboot.entities.Eleitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.matheusob25.sistemavotacaospringboot.services.EleitorService;
@@ -21,5 +22,10 @@ public class EleitorResource {
     public ResponseEntity<List<Eleitor>> listarEleitores(){
         List<Eleitor> eleitores = eleitorService.listarEleitores();
         return ResponseEntity.ok().body(eleitores);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Eleitor> buscarEleitorPorId(@PathVariable Long id){
+        Eleitor eleitor = eleitorService.buscarEleitorPorId(id);
+        return ResponseEntity.ok().body(eleitor);
     }
 }
--- src/main/java/com/matheusob25/sistemavotacaospringboot/resources/PoliticoResource.java
+++ src/main/java/com/matheusob25/sistemavotacaospringboot/resources/PoliticoResource.java
@@ -1,16 +1,34 @@
 package com.matheusob25.sistemavotacaospringboot.resources;
 
 
+import com.matheusob25.sistemavotacaospringboot.entities.Politico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.matheusob25.sistemavotacaospringboot.services.PoliticoService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/politicos")
 public class PoliticoResource {
 
+    @Autowired
     private PoliticoService politicoService;
 
+    @GetMapping
+    public ResponseEntity<List<Politico>> listarPoliticos() {
+        List<Politico> politicos = politicoService.listarPoliticos();
+        return ResponseEntity.ok().body(politicos);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Politico> buscarPoliticoPorId(@PathVariable Long id) {
+        Politico politico = politicoService.encontrarPoliticoPorId(id);
+        return ResponseEntity.ok().body(politico);
+    }
 
 
 }
